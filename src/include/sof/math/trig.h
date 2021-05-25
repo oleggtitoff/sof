56d5d925235d4c8c67f23e495e968d02f332f602
ShriramShastry <malladi.sastry@intel.com>
2021-05-25 20:13:22 +0530

    Math: Trignometry: Add arc sine, cosine, cexp fun
    Added new trignometric cordic approx based arcsine,
    arc cosine and complex exponential function
    
    Signed-off-by: ShriramShastry <malladi.sastry@intel.com>

--- src/include/sof/math/trig.h
+++ src/include/sof/math/trig.h
@@ -5,6 +5,7 @@
  * Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
  *         Liam Girdwood <liam.r.girdwood@linux.intel.com>
  *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Shriram Shastry <malladi.sastry@linux.intel.com>
  */
 
 #ifndef __SOF_MATH_TRIG_H__
@@ -21,17 +22,31 @@
 #define PI_Q4_28      843314857
 #define PI_MUL2_Q4_28     1686629713
 #define CORDIC_31B_TABLE_SIZE		31
-#define CORDIC_SIN_COS_15B_TABLE_SIZE	15
+#define CORDIC_15B_TABLE_SIZE		15
+#define CORDIC_30B_ITABLE_SIZE		30
+#define CORDIC_15B_ITABLE_SIZE		15
 
 typedef enum {
 	EN_32B_CORDIC_SINE,
 	EN_32B_CORDIC_COSINE,
+	EN_32B_CORDIC_CEXP,
 	EN_16B_CORDIC_SINE,
 	EN_16B_CORDIC_COSINE,
+	EN_16B_CORDIC_CEXP,
+	EN_32B_CORDIC_ASINE,
+	EN_32B_CORDIC_ACOSINE,
 } cordic_cfg;
 
-void cordic_sin_cos(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t *b_yn, int32_t *xn,
-		    int32_t *th_cdc_fxp);
+static const int32_t cordic_ilookup[CORDIC_30B_ITABLE_SIZE] = {
+	    497837829, 263043836, 133525158, 67021686, 33543515, 16775850,
+	    8388437,   4194282,	  2097149,   1048575,  524287,	 262143,
+	    131071,    65535,	  32767,     16383,    8191,	 4095,
+	    2047,      1023,	  511,	     255,      127,	 63,
+	    31,	       15,	  8,	     4,	       2,	 1};
+void cordic_approx(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t *b_yn, int32_t *xn,
+		   int32_t *th_cdc_fxp);
+int32_t iscalarcordic(int32_t realvalue, int16_t numiters,
+		      const int32_t LUT[CORDIC_30B_ITABLE_SIZE], cordic_cfg type);
 /* Input is Q4.28, output is Q1.31 */
 /**
  * Compute fixed point cordicsine with table lookup and interpolation
@@ -50,12 +65,12 @@ void cordic_sin_cos(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t 
  */
 static inline int32_t sin_fixed_32b(int32_t th_rad_fxp)
 {
+	int32_t th_cdc_fxp;
 	int32_t sign;
 	int32_t b_yn;
 	int32_t xn;
-	int32_t th_cdc_fxp;
 	cordic_cfg type = EN_32B_CORDIC_SINE;
-	cordic_sin_cos(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+	cordic_approx(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
 
 	th_cdc_fxp = sign * b_yn;
 	/*convert Q2.30 to Q1.31 format*/
@@ -78,18 +93,17 @@ static inline int32_t sin_fixed_32b(int32_t th_rad_fxp)
  */
 static inline int32_t cos_fixed_32b(int32_t th_rad_fxp)
 {
+	int32_t th_cdc_fxp;
 	int32_t sign;
 	int32_t b_yn;
 	int32_t xn;
-	int32_t th_cdc_fxp;
 	cordic_cfg type = EN_32B_CORDIC_COSINE;
-	cordic_sin_cos(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+	cordic_approx(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
 
 	th_cdc_fxp = sign * xn;
 	/*convert Q2.30 to Q1.31 format*/
 	return sat_int32(Q_SHIFT_LEFT((int64_t)th_cdc_fxp, 30, 31));
 }
-
 /* Input is Q4.28, output is Q1.15 */
 /**
  * Compute fixed point cordic sine with table lookup and interpolation
@@ -108,13 +122,14 @@ static inline int32_t cos_fixed_32b(int32_t th_rad_fxp)
  */
 static inline int16_t sin_fixed_16b(int32_t th_rad_fxp)
 {
+	int32_t th_cdc_fxp;
 	int32_t sign;
 	int32_t b_yn;
 	int32_t xn;
-	int32_t th_cdc_fxp;
 	cordic_cfg type = EN_16B_CORDIC_SINE;
 	/* compute coeff from angles*/
-	cordic_sin_cos(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+	cordic_approx(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+
 	th_cdc_fxp = sign * b_yn;
 	/*convert Q1.31 to Q1.15 format*/
 	return sat_int16(Q_SHIFT_RND((sat_int32(Q_SHIFT_LEFT((int64_t)th_cdc_fxp, 30, 31))),
@@ -137,17 +152,137 @@ static inline int16_t sin_fixed_16b(int32_t th_rad_fxp)
  */
 static inline int16_t cos_fixed_16b(int32_t th_rad_fxp)
 {
+	int32_t th_cdc_fxp;
 	int32_t sign;
 	int32_t b_yn;
 	int32_t xn;
-	int32_t th_cdc_fxp;
 	cordic_cfg type = EN_16B_CORDIC_COSINE;
 	/* compute coeff from angles*/
-	cordic_sin_cos(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+	cordic_approx(th_rad_fxp, type, &sign, &b_yn, &xn, &th_cdc_fxp);
+
 	th_cdc_fxp = sign * xn;
 	/*convert Q1.31 to Q1.15 format*/
 	return sat_int16(Q_SHIFT_RND((sat_int32(Q_SHIFT_LEFT((int64_t)th_cdc_fxp, 30, 31))),
 	31, 15));
+}
+
+/**
+ * CORDIC-based approximation of inverse cosine
+ * inverse cosine of cdc_acos_th based on a CORDIC approximation
+ * Inverse cosine angle values in rad
+ * Q2.30 cdc_acos_th , input value range [-1 to 1]
+ * Q2.30 th_rad_fxp, output value range [3.14159265346825 to 0]
+ * LUT size set type 31
+ * Error (max = 0.000000026077032), THD+N  = -157.948952635422842
+ */
+static inline void acosine_fixed_32b(int32_t cdc_acos_th, int32_t th_rad_fxp)
+{
+	int32_t saturatedUnaryMinus;
+
+	cordic_cfg type = EN_32B_CORDIC_ACOSINE;
+
+	saturatedUnaryMinus = cdc_acos_th;
+	if (saturatedUnaryMinus >= 0) {
+		th_rad_fxp = iscalarcordic(saturatedUnaryMinus, CORDIC_31B_TABLE_SIZE,
+					   cordic_ilookup, type);
+	} else {
+		if (saturatedUnaryMinus <= INT32_MIN)
+			saturatedUnaryMinus = INT32_MAX;
+		else
+			saturatedUnaryMinus = -saturatedUnaryMinus;
+
+	    th_rad_fxp =
+		PI_MUL2_Q4_28 - iscalarcordic(saturatedUnaryMinus, CORDIC_31B_TABLE_SIZE,
+		cordic_ilookup, type);
+	}
+}
+
+/**
+ * CORDIC-based approximation of inverse cosine
+ * inverse cosine of cdc_acos_th based on a CORDIC approximation
+ * Inverse cosine angle values in rad
+ * Q2.30 cdc_acos_th , input value range [-1 to 1]
+ * Q2.30 th_rad_fxp, output value range [3.14159265346825 to 0]
+ * LUT size set type 15
+ * Error (max = 0.000000026077032), THD+N  = -157.948952635422842
+ */
+static inline void acosine_fixed_16b(int32_t cdc_acos_th, int32_t th_rad_fxp)
+{
+	int32_t saturatedUnaryMinus;
+	cordic_cfg type = EN_32B_CORDIC_ACOSINE;
+
+	saturatedUnaryMinus = cdc_acos_th;
+	if (saturatedUnaryMinus >= 0) {
+		th_rad_fxp = iscalarcordic(saturatedUnaryMinus, CORDIC_15B_ITABLE_SIZE,
+					   cordic_ilookup, type);
+	} else {
+		if (saturatedUnaryMinus <= INT32_MIN)
+			saturatedUnaryMinus = INT32_MAX;
+		else
+			saturatedUnaryMinus = -saturatedUnaryMinus;
+
+		th_rad_fxp =
+		PI_MUL2_Q4_28 - iscalarcordic(saturatedUnaryMinus, CORDIC_15B_ITABLE_SIZE,
+		cordic_ilookup, type);
+	}
+}
+
+/**
+ * CORDIC-based approximation of inverse sine
+ * inverse sine of cdc_asin_th based on a CORDIC approximation.
+ * Inverse sine angle values in rad
+ * Q2.30 cdc_asin_th,input value range [-1 to 1]
+ * Q2.30 th_rad_fxp, output value range [-1.5707963258028 to 1.5707963258028]
+ * LUT size set type 31
+ * Error (max = 0.000000027939677), THD+N  = -157.454534077921551
+ */
+static inline void asin_fixed_32b(int32_t cdc_asin_th, int32_t th_rad_fxp)
+{
+	int32_t saturatedUnaryMinus;
+	cordic_cfg type = EN_32B_CORDIC_ASINE;
+
+	saturatedUnaryMinus = cdc_asin_th;
+	if (saturatedUnaryMinus >= 0) {
+		th_rad_fxp = iscalarcordic(saturatedUnaryMinus, CORDIC_31B_TABLE_SIZE,
+					   cordic_ilookup, type);
+	} else {
+		if (saturatedUnaryMinus <= INT32_MIN)
+			saturatedUnaryMinus = INT32_MAX;
+		else
+			saturatedUnaryMinus = -saturatedUnaryMinus;
+
+	th_rad_fxp = -iscalarcordic(saturatedUnaryMinus, CORDIC_31B_TABLE_SIZE,
+				    cordic_ilookup, type);
+	}
+}
+
+/**
+ * CORDIC-based approximation of inverse sine
+ * inverse sine of cdc_asin_th based on a CORDIC approximation.
+ * Inverse sine angle values in rad
+ * Q2.30 cdc_asin_th, value in between range of [-1 to 1]
+ * Q2.30 th_rad_fxp, output value range [-1.5707963258028 to 1.5707963258028]
+ * LUT size set type 15
+ * Error (max = 0.000000027939677), THD+N  = -157.454534077921551
+ */
+static inline void asin_fixed_16b(int32_t cdc_asin_th, int32_t th_rad_fxp)
+{
+	int32_t saturatedUnaryMinus;
+	cordic_cfg type = EN_32B_CORDIC_ASINE;
+
+	saturatedUnaryMinus = cdc_asin_th;
+	if (saturatedUnaryMinus >= 0) {
+		th_rad_fxp = iscalarcordic(saturatedUnaryMinus, CORDIC_15B_ITABLE_SIZE,
+					   cordic_ilookup, type);
+	} else {
+		if (saturatedUnaryMinus <= INT32_MIN)
+			saturatedUnaryMinus = INT32_MAX;
+		else
+			saturatedUnaryMinus = -saturatedUnaryMinus;
+
+	    th_rad_fxp = -iscalarcordic(saturatedUnaryMinus, CORDIC_15B_ITABLE_SIZE,
+	    cordic_ilookup, type);
+	}
 }
 
 #endif /* __SOF_MATH_TRIG_H__ */
--- src/math/trig.c
+++ src/math/trig.c
@@ -32,12 +32,30 @@ const int32_t cordic_sine_cos_piovertwo_q30fl  = Q_CONVERT_FLOAT(_M_PI / 2, 30);
 const int32_t cord_sincos_piovertwo_q28fl  = Q_CONVERT_FLOAT(_M_PI / 2, 28);
 /* 843314857,  deg = 90.000000	*/
 const int32_t cord_sincos_piovertwo_q29fl  = Q_CONVERT_FLOAT(_M_PI / 2, 29);
+/* arc trignometry constant*/
+/**
+ * \+----------+----------------------------------------+--------------------+-------------------+
+ * \|values    |		Q_CONVERT_FLOAT		|(180/pi)*angleInRad |(pi/180)*angleInDeg|
+ * \+----------+----------------------------------------+--------------------+-------------------+
+ * \|379625062 | Q_CONVERT_FLOAT(1.4142135605216026, 28)|    81.0284683480568| 1.41421356052160  |
+ * \|1073741824| Q_CONVERT_FLOAT(1.0000000000000000, 30)|    57.2957795130823| 1.00000000000000  |
+ * \|843314856 | Q_CONVERT_FLOAT(1.5707963258028030, 29)|    89.9999999431572| 1.57079632580280  |
+ * \|1686629713| Q_CONVERT_FLOAT(1.5707963267341256, 30)|    89.9999999965181| 1.57079632673413  |
+ * \+----------+----------------------------------------+--------------------+-------------------+
+ */
+/* 379625062,  deg = 81.0284683480568475 or round(1.4142135605216026*2^28) */
+const int32_t cord_arcsincos_q28fl  = Q_CONVERT_FLOAT(1.4142135605216026 / 2, 28);
+/* 843314856 , deg = 89.9999999431572348 or round(pi/2*2^29) */
+/* 843314857, Bug in calculation, so using hardcoded value */
+const int32_t cord_arcsincos_piovertwo_q29fl  = Q_CONVERT_FLOAT(_M_PI / 2, 29);
+/* 1073741824, deg = 57.2957795130823229 or round(1*2^30)*/
+const int32_t cord_arcsincos_q30fl  = Q_CONVERT_FLOAT(1.0000000000000000, 30);
 
 /**
  * \CORDIC-based approximation of sine and cosine
  */
-void cordic_sin_cos(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t *b_yn, int32_t *xn,
-		    int32_t *th_cdc_fxp)
+void cordic_approx(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t *b_yn, int32_t *xn,
+		   int32_t *th_cdc_fxp)
 {
 	int32_t b_idx;
 	int32_t xtmp;
@@ -74,8 +92,10 @@ void cordic_sin_cos(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t 
 	xtmp = cordic_sine_cos_lut_q29fl;
 	ytmp = 0;
 
-	if ((type == EN_16B_CORDIC_SINE) || (type == EN_16B_CORDIC_COSINE))
-		a_idx = CORDIC_SIN_COS_15B_TABLE_SIZE;
+	if (type == EN_16B_CORDIC_SINE ||
+	    type == EN_16B_CORDIC_COSINE ||
+	    type == EN_16B_CORDIC_CEXP)
+		a_idx = CORDIC_15B_TABLE_SIZE;
 
 	/* Calculate the correct coefficient values from rotation angle.
 	 * Find difference between the coefficients from the lookup table
@@ -94,9 +114,113 @@ void cordic_sin_cos(int32_t th_rad_fxp, cordic_cfg type, int32_t *sign, int32_t 
 		xtmp = *xn >> (b_idx + 1);
 		ytmp = *b_yn >> (b_idx + 1);
 	}
-	/* Q2.30 format */
+	/* Q2.30 format -sine,cosine*/
 	*th_cdc_fxp = th_rad_fxp;
+}
 
+/**
+ * Arguments	: int32_t realvalue
+ *		  int16_t numiters
+ *		  const int32_t LUT[CORDIC_30B_ITABLE_SIZE][30]
+ * Return Type	: int32_t
+ */
+int32_t iscalarcordic(int32_t realvalue, int16_t numiters,
+		      const int32_t LUT[CORDIC_30B_ITABLE_SIZE], cordic_cfg type)
+{
+	int32_t b_sincosvalue;
+	int32_t b_i;
+	int32_t i;
+	int32_t x = 0;
+	int32_t xdshift;
+	int32_t xshift;
+	int32_t y = 0;
+	int32_t ydshift;
+	int32_t yshift;
+	int32_t z = 0;
+	int16_t j;
+	int16_t k;
+
+	b_sincosvalue = (realvalue >> 1U);
+	if (type == EN_32B_CORDIC_ASINE) {
+	/*ternary operator*/
+		if (b_sincosvalue > cord_arcsincos_q28fl) {
+			x = 0;
+			y = cord_arcsincos_q30fl;
+			z = 843314856;
+		} else {
+			x = cord_arcsincos_q30fl;
+			y = 0;
+			z = 0;
+		}
+	}
+	if  (type == EN_32B_CORDIC_ACOSINE) {
+	/*ternary operator*/
+		if (b_sincosvalue < cord_arcsincos_q28fl) {
+			x = 0;
+			y = cord_arcsincos_q30fl;
+			z = 843314856;
+		} else {
+			x = cord_arcsincos_q30fl;
+			y = 0;
+			z = 0;
+		}
+	}
+	b_sincosvalue <<= 1U;
+	/*DCORDIC(Double CORDIC) algorithm*/
+	i = (int32_t)((int16_t)(numiters - 1));
+	for (b_i = 0; b_i < i; b_i++) {
+		j = (int16_t)((b_i + 1) << 1U);
+		if (j >= 31)
+			j = 31;
+
+		if (b_i < 31)
+			k = (int16_t)b_i;
+		else
+			k = 31;
+
+		xshift = (x >>	(k));
+		xdshift = (x >> (j));
+		yshift = (y >>	(k));
+		ydshift = (y >> (j));
+
+		/*Adjust input with coefficients*/
+		if (type == EN_32B_CORDIC_ASINE) {
+			if (y == b_sincosvalue) {
+				x += xdshift;
+				y += ydshift;
+			} else if (((y >= b_sincosvalue) && (x >= 0)) ||
+				  ((y < b_sincosvalue) && (x < 0))) {
+				x = (x - xdshift) + yshift;
+				y = (y - ydshift) - xshift;
+				z -= LUT[b_i];
+			} else {
+				x = (x - xdshift) - yshift;
+				y = (y - ydshift) + xshift;
+				z += LUT[b_i];
+			}
+		}
+		if (type == EN_32B_CORDIC_ACOSINE) {
+			if (x == b_sincosvalue) {
+				x += xdshift;
+				y += ydshift;
+			} else if (((x > b_sincosvalue) && (y >= 0)) ||
+				  ((x < b_sincosvalue) && (y < 0))) {
+				x = (x - xdshift) - yshift;
+				y = (y - ydshift) + xshift;
+				z += LUT[b_i];
+			} else {
+				x = (x - xdshift) + yshift;
+				y = (y - ydshift) - xshift;
+				z -= LUT[b_i];
+			}
+		}
+
+		b_sincosvalue += (b_sincosvalue >> ((uint32_t)j));
+	}
+	if (z < 0)
+		z = -z;
+
+	return z;
 }
 
 #endif
